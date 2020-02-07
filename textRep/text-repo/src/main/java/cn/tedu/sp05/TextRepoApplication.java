90758e3e2284cad53497a6a6bbe0320f5c8e5cf8
hezhiyuan1061144386 <hezhiyuan1061144386@PTOP-2KPAHDOT.mshome.net>
2020-02-07 11:19:14 +0800

    1

---
+++ textRep/text-repo/src/main/java/cn/tedu/sp05/TextRepoApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextRepoApplication.class, args);
+	}
+
+}
